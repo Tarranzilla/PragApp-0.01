c57fbeca9b6a55f5f94067dfe8b41ca0cac333ac
JOAO TARRAN <joaotarran@gmail.com>
2023-02-09 14:28:27 -0300

    Update

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 /* ================ Dependencias ================ */
-
+const env = require("dotenv").config();
 const path = require("path");
 const express = require("express");
+const mongodb = require("mongodb");
 //const cors = require("cors");
 
 /* App se torna uma instancia de express */
 const app = express();
+
+const connectionPassword = process.env.CONNECTIONPASSWORD;
+const connectionString =
+    "mongodb+srv://pragmata:" +
+    connectionPassword +
+    "@cluster0.gxrvnnw.mongodb.net/Database_001?retryWrites=true&w=majority";
+mongodb.connect(
+    connectionString,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Connected to Database");
+            db = client.db();
+        }
+    }
+);
 
 /* ================ Configurações ================ */
 //app.use(express.urlencoded({ extended: false }));
